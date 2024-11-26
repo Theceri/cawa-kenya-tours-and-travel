c0482282ba9e94ac4a7d427cfc0fa05696f76ddd
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-11-26 10:38:22 +0300

    Vacations complete

--- src/app/vacation/vacationsData.js
+++ src/app/vacation/vacationsData.js
@@ -174,7 +174,121 @@ const vacationsData = [
     `,
         "slug": "7-days-masai-mara-lake-naivasha-and-amboseli",
         "duration": "7 Days/6 Nights"
+    },
+
+    {
+        title: "7 Days - Masai Mara and Lake Naivasha",
+        subtitle: "In this tour we will cover the 2 most popular parks in Kenya - Masai Mara National Reserve (5 nights) & Lake Naivasha (1 night). Masai Mara is home for Big Five (elephants, lions, leopards, rhinos, and buffalo) & also zebras, antelope, hyenas, giraffes, African lions including black-maned lion. Lake Naivasha is a beautiful freshwater lake in Kenya which lies just north-west of Nairobi.",
+        image: "/images/vacations/7-days-masai-mara-lake-naivasha.jpg",
+        content: `
+        <h3>Day 1 - Nairobi to Masai Mara</h3>
+        <p>Your tour starts with pick up at 7:30 AM from your Nairobi hotel or Airport and drive to Masai Mara. This drive will take about 5 to 6.5 hours with a scenic stopover at Great Rift Valley. Arrive in Masai Mara shortly after noon and check into your lodge/camp, have hot lunch and then depart for an afternoon game drive.</p>
+        <p>After the game drive, we head back to lodge/camp to reach by around 5:30 or 6:00 PM, freshen up and enjoy a freshly prepared Dinner. Overnight at your lodge.</p>
+        <p><strong>Note:</strong> The last 1-hour drive to Masai Mara is on the corrugated and bumpy road and may not be comfortable for clients with back problems. Clients with back problems can consider flying directly to (and out of) Masai Mara.</p>
+
+        <h3>Day 2, Day 3, Day 4 & Day 5 - Masai Mara</h3>
+        <p>Today's itinerary is flexible and you can discuss with your guide on the previous evening and plan for today's schedule.</p>
+        <p>You can either have a relaxed breakfast and depart for full day game drives around 9:00 AM with packed lunch and return back to lodge late afternoon around 6:00 PM. Or you can depart at 6:00 AM on an early morning game drive (This is the best time to see the animals and a good chance to witness hunt/kill). Later return to accommodation for breakfast and relax for a bit and depart on an afternoon game drive with packed lunch. You will stop at a picnic spot for having packed lunch and later depart on a game drive till the park is closed. Around 6:00 PM return to lodge for dinner and overnight stay.</p>
+
+        <h3>Day 6 - Masai Mara to Lake Naivasha</h3>
+        <p>After breakfast, checkout and depart to Lake Naivasha which is about a 5-hour drive.</p>
+        <p>Check-in to your camp/lodge in Naivasha for dinner and overnight stay.</p>
+        <p><strong>Note:</strong> Morning game drives in Masai Mara can be added on the last morning at the park by paying park fees (USD 100 from Jan to Jun and USD 200 from Jul to Dec).</p>
+
+        <h3>Day 7 - Lake Naivasha to Nairobi</h3>
+        <p>After breakfast, proceed to Lake Naivasha for a boat ride to Crescent Island where you will be doing a nature walk next to zebras, giraffes, wildebeest, waterbucks, antelopes, and many types of birds. Return by boat to Naivasha for lunch at your lodge and then proceed to Nairobi.</p>
+    `,
+        slug: "7-days-masai-mara-lake-naivasha",
+        duration: "7 Days/6 Nights",
+    },
+
+
+
+
+    {
+        title: "2 Days (Coast) - Tsavo East National Park",
+        subtitle: "On this 2-day tour starting and ending at Coastal Kenya, immerse yourself in the vast wilderness teeming with wildlife of Tsavo East. Experience exciting game drives where you can spot the Big 5 and other unique species. Explore the stunning landscapes, from red soil plains to acacia woodlands. This adventure allows you to connect with nature and enjoy unforgettable wildlife encounters in one of Kenya’s largest national parks, making it a perfect getaway for wildlife enthusiasts.",
+        image: "/images/vacations/2-days-coast-tsavo-east-national-park.jpg",
+        content: `
+        <h3>Day 1 - Nairobi to Mombasa/Diani</h3>
+        <p>Your tour starts with pick up at 7:30 AM from your Mombasa/Diani hotel or Mombasa/Diani Airport and drive to Tsavo East.</p>
+        <p>After the game drive, head back to lodge/camp to reach by around 5:30 or 6:00 PM, freshen up and enjoy a freshly prepared Dinner. Overnight at your lodge.</p>
+
+        <h3>Day 2 - Tsavo East to Mombasa/Diani</h3>
+        <p>Wake up early and do Morning game drives at Tsavo East. After a relaxed breakfast head back to Mombasa/Diani. On reaching Mombasa/Diani by the afternoon, you will be dropped off at the airport or your hotel.</p>
+    `,
+        slug: "2-days-coast-tsavo-east-national-park",
+        duration: "2 Days/1 Night",
+    },
+
+
+    {
+        title: "4 Days - Samburu National Reserve",
+        subtitle: "This 4-day tour of Samburu National Reserve offers a unique wildlife experience in Kenya's northern region. Enjoy thrilling game drives as you search for the Big 5 and the Special 5 animals. Explore the stunning landscapes characterized by rolling hills and riverine forests. This adventure promises unforgettable wildlife encounters and breathtaking views, making it perfect for nature enthusiasts seeking a distinctive safari experience.",
+        image: "/images/vacations/4-days-samburu-national-reserve.jpg",
+        content: `
+        <h3>Day 1 - Nairobi to Samburu</h3>
+        <p>Your tour starts with pick up at 7:00 AM from your Nairobi hotel or Airport and drive to Samburu. This drive will take about 6 to 7 hours with diverse views ranging from the raw countryside, the central highlands of Kenya, the equator line, and views of Mt Kenya. Arrive in Samburu by lunchtime and check into your lodge/camp, have a hot lunch and then depart for an afternoon game drive. After the game drive, we head back to lodge/camp to reach by around 5:30 or 6:00 PM, freshen up and enjoy a freshly prepared Dinner. Overnight at your lodge.</p>
+        <p><strong>Note:</strong> Samburu ecosystem consists of 3 national reserves: Samburu National Reserve, Buffalo Springs National Reserve, and Shaba National Reserve. Entry fees are common for Buffalo Springs and Shaba but different for Samburu. If your accommodation is in Samburu, your game drives are included in Samburu. If your accommodation is in Buffalo Springs or Shaba, your game drives are included in Buffalo Springs + Shaba. If your accommodation is in Buffalo Springs or Shaba and you wish to enter Samburu Reserve, park fees of USD 80 per adult and USD 45 per child apply.</p>
+
+        <h3>Day 2 - Samburu</h3>
+        <p>Today's itinerary is flexible and you can discuss with your guide on the previous evening and plan for today's schedule. We depart at 6:30 AM after breakfast on an early morning game drive (This is the best time to see the animals and a good chance to witness a hunt/kill). Later around 10:30 AM, return to accommodation for a break and relax for a bit and after lunch, depart on an afternoon game drive around 3:00 PM till around 6:00 PM. Return to lodge for dinner and overnight stay.</p>
+
+        <h3>Day 3 - Samburu</h3>
+        <p>Today's itinerary is flexible and you can discuss with your guide on the previous evening and plan for today's schedule. We depart at 6:30 AM after breakfast on an early morning game drive (This is the best time to see the animals and a good chance to witness a hunt/kill). Later around 10:30 AM, return to accommodation for a break and relax for a bit and after lunch, depart on an afternoon game drive around 3:00 PM till around 6:00 PM. Return to lodge for dinner and overnight stay.</p>
+
+        <h3>Day 4 - Samburu to Nairobi</h3>
+        <p>After breakfast, checkout and depart for morning game drives with packed lunch till around 1:00 PM and then proceed to Nairobi where we will reach around 4:00 PM at your hotel or airport in Nairobi. This drive will take around 6 - 7 hours.</p>
+    `,
+        slug: "4-days-samburu-national-reserve",
+        duration: "4 Days/3 Nights",
+    },
+
+    {
+        title: "6 days - Masai Mara, Central Serengeti & Ngorongoro Crater",
+        subtitle: `In this tour we will cover the most popular destination in Kenya - Masai Mara National Reserve (2 nights). Tour starts in Nairobi and ends at Isebania. With our experienced driver guides, it will be a trip of a lifetime! In this tour we will cover the 2 most popular destinations in Tanzania - Central Serengeti(2 nights) and Ngorongoro Crater(1 night).Tour starts at Isebania and ends in Arusha.With our experienced driver guides, it will be a trip of a lifetime!`,
+        image: "/images/vacations/6-days-masai-mara-central-serengeti-ngorongoro-crater.jpg",
+        content: `
+        <h3>Day 1 - Nairobi to Masai Mara</h3>
+        <p>Your tour starts with pick up at 7:30 AM from your Nairobi hotel or Airport and drive to Masai Mara. This drive will take about 5 to 6.5 hours with a scenic stopover at Great Rift Valley. Arrive in Masai Mara shortly after noon and check into your lodge/camp, have hot lunch and then depart for an afternoon game drive.</p>
+        <p>After the game drive, we head back to lodge/camp to reach by around 5:30 or 6:00 PM, freshen up and enjoy a freshly prepared Dinner. Overnight at your lodge.</p>
+        <p><strong>Note:</strong> The last 1-hour drive to Masai Mara is on the corrugated and bumpy road and may not be comfortable for clients with back problems. Clients with back problems can consider flying directly to (and out of) Masai Mara.</p>
+        
+        <h3>Day 2 - Masai Mara</h3>
+        <p>Today's itinerary is flexible and you can discuss with your guide on the previous evening and plan for today's schedule.</p>
+        <p>You can either have a relaxed breakfast and depart for full day game drives around 9:00 AM with packed lunch and return back to lodge late afternoon around 6:00 PM. Or you can depart at 6:00 AM on an early morning game drive (This is the best time to see the animals and a good chance to witness hunt/kill). Later return to accommodation for breakfast and relax for a bit and depart on an afternoon game drive with packed lunch. you will stop at a picnic spot for having packed lunch and later depart on a game drive till the park is closed. Around 6:00 PM return to lodge for dinner and overnight stay.</p>
+
+        <h3>Day 3 - Masai Mara to Isebania</h3>
+        <p>Enjoy your breakfast at the lodge. Afterward, drive 3.5 to 4 hours (150 km) to the Isebania border with a packed lunch.</p>
+        <p><strong>Note 1:</strong> Effective from 1st November 2023, our 2024 prices have been updated to reflect the revised park fees for Masai Mara (USD 100 from Jan to Jun and USD 200 from July to Dec).</p>
+        <p><strong>Note 2:</strong> Morning game drives in Masai Mara can be added on the last morning at the park by paying additional park fees.</p>
+
+        <h3>Day 4 - Isebania to Central Serengeti</h3>
+        <p>Upon completing your immigration formalities at the Isebania border, our guide/driver will meet you around 11:00-11:30 AM. The journey from the Isebania border to Central Serengeti takes approximately 4.5 hours.</p>
+        <p>Along the way, you'll have the opportunity to view picturesque villages and lush farmlands before entering the Serengeti National Park. You will have the opportunity to enjoy lunch at Ikoma Gate while your guide finalizes the park entry formalities. As you traverse the park, you'll enjoy en-route game drives, with your arrival at the accommodation expected between 5:00-5:30 PM. The day concludes with a hearty dinner and a restful night at your accommodation.</p>
+
+        <h3>Day 5 - Central Serengeti</h3>
+        <p>Begin your day with a leisurely breakfast before setting out for a full-day game drive around 8:00 AM. Equipped with a packed lunch, you will explore the central Serengeti and return to the lodge in the late afternoon, around 5:00 PM.</p>
+        <p>Alternatively, embark on an early morning game drive at 6:00 AM. After returning to the lodge for breakfast, enjoy some relaxation before heading out for an afternoon game drive, with a packed lunch provided.</p>
+        <p>Your exploration will take you through the Central Serengeti, particularly the Seronera area, known for its rich wildlife habitat and the Seronera River, which draws a diverse array of species (Three of the big cats: Lions, Cheetahs, and Leopards. Other predators like hyenas, wild dogs, wildcats, caracals, etc. And other animals like elephants, buffalos, and giraffes). This region is famed for its large lion prides and offers exceptional game viewing opportunities year-round.</p>
+        <p>You can enjoy your meal at a scenic picnic spot at your preferred time and continue exploring the central Serengeti.</p>
+        <p>The day will conclude around 5:00 PM, followed by a hearty dinner and a restful night at your accommodation.</p>
+
+        <h3>Day 6 - Central Serengeti to Ngorongoro</h3>
+        <p>After early breakfast, embark on a morning game drive in Serengeti National Park, immersing yourself in its vibrant wildlife. Around noon, depart for the Ngorongoro Conservation Area, with a picnic lunch provided for your journey. The transfer to your accommodation in Ngorongoro/Karatu takes approximately 3.5 to 4 hours.</p>
+        <p>The route begins with a scenic drive through the Serengeti, where you may encounter gazelles and impalas. Before you exit Serengeti and enter the Ngorongoro Conservation Area, you can enjoy a picnic lunch at Naabi Hill Gate and also go for a short hike to the top of the hill for magnificent views of the Serengeti plains. As you progress, the landscape transitions to a drier, somewhat dusty terrain, with occasional Maasai villages along the way. After another 1.5 hours, you will ascend into the Ngorongoro Highlands, where the climate becomes cooler and more pleasant.</p>
+        <p>The day concludes with a hearty dinner and a restful night at your accommodation, reflecting on the extraordinary experiences of the day.</p>
+
+        <h3>Day 7 - Ngorongoro Crater Tour (5 Hours) to Arusha</h3>
+        <p>On the final day of your safari, you will start early with a quick breakfast before descending into the Ngorongoro Crater around 6:30 AM (you might reach Crater floor by 7:00 AM if you sleep in Karatu the previous night). The Ngorongoro Crater, the world’s largest collapsed volcanic caldera, encompasses approximately 260 square kilometers of pristine natural beauty. Surrounded by a ring of extinct volcanoes, the crater floor, which is adorned with various watering holes, supports a diverse population of around 25,000 animals. With depths exceeding 2,000 feet, this remarkable caldera offers a unique and breathtaking landscape.</p>
+        <p>A 5-hour game drive on the crater floor offers a chance to see diverse wildlife, including African elephants, buffalo, black rhinos, hippos, hyenas, cheetahs, and lions. Keep your camera ready to capture the action. After a picnic lunch at the scenic Hippo Pool, you will begin your ascent out of the crater. The drive to Arusha showcases varied landscapes, starting from Ngorongoro's highlands and descending through rugged terrain, farmland, and Maasai villages. The well-maintained road winds through hills and forests before opening up to expansive grassy plains and distant mountains.</p>
+        <p>You will complete your journey with a 4-hour drive to Arusha, arriving at your preferred location by 5:30 PM. As your safari ends, you’ll leave with unforgettable experiences and cherished memories of Tanzania’s stunning landscapes and rich cultures.</p>
+    `,
+        slug: "6-days-masai-mara-central-serengeti-ngorongoro-crater",
+        duration: "6 Days/5 Nights",
     }
+
+
 
     // Add more vacation objects as needed
 ];
